43f8da1e6a94f134120bf3f6b73368e246a73187
Wes Bourke <wesbourkeavl@gmail.com>
2022-05-24 23:57:27 -0400

    added destory action on the backend

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,16 +1,24 @@
 class RoutesController < ApplicationController
+    before_action :beforeRoute, only: [:destroy]
     def index
         render json: Route.all, status: :ok
     end
-
     def create
-     route = Route.create!(var_params)
-     render json: route, status: :created
+        route = Route.create!(var_params)
+        render json: route, status: :created
+    end
+    def destroy
+        @findRoute.destroy
+        head :no_content
+        # render json: @findRoute, status :accepted
     end
     
     private
-    #goes under private
     def var_params
-     params.permit(:approved, :description, :latitude, :longitude, :route_name, :stars)
+        params.permit(:approved, :description, :latitude, :longitude, :route_name, :stars)
+    end
+    #to be used with show,update and destroy
+    def beforeRoute
+        @findRoute = Route.find(params[:id])
     end
 end
